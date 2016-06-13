b40528479b47955edd923925bddd75d14242a719
WuHaolin <gwuhaolin@gmail.com>
2016-06-13 15:44:32 +0800

    add test case

--- src/main/java/cn/wuhaolin/repository/StudentRepo.java
+++ src/main/java/cn/wuhaolin/repository/StudentRepo.java
@@ -1,9 +1,18 @@
 package cn.wuhaolin.repository;
 
 import cn.wuhaolin.entity.Student;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource
 public interface StudentRepo extends JpaRepository<Student, Long> {
+    List<Student> ageGreaterThan(@Param("age") int age, Pageable pageable);
+
+    List<Student> nameContaining(@Param("keyword") String keyword);
+
+    Student nameEquals(@Param("name") String name);
 }
