f962b1dd63b6f00bd574beb12bb1279a40b2ceb2
WuHaolin <gwuhaolin@gmail.com>
2016-06-13 10:48:37 +0800

    add test case

--- src/main/java/cn/wuhaolin/controller/TestCtrl.java
+++ src/main/java/cn/wuhaolin/controller/TestCtrl.java
@@ -5,11 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/")
 public class TestCtrl {
 
     @RequestMapping("returnString")
     @ResponseBody
     public String returnString() {
         return "string";
+    }
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String index() {
+        return "hi";
     }
 }
